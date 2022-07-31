5159d621215570652c0d14ea0bb0b6634d48a62d
LeandroSimo <leo.bt.12@hotmail.com>
2022-07-31 20:58:11 -0300

    Exercise list card

--- lib/app/modules/exercise/view/exercise_page.dart
+++ lib/app/modules/exercise/view/exercise_page.dart
@@ -28,7 +28,36 @@ class ExercisePage extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 80),
+            child: Card(
+              child: ListTile(
+                title: const Text(
+                  '40 minutos de corrida',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Text(
+                  'Velocidade constante de corrida por 40 minutos',
+                  style: TextStyle(
+                    color: Theme.of(context).textTheme.subtitle2?.color,
+                  ),
+                ),
+                leading: Image.network(
+                  'https://img.icons8.com/bubbles/2x/timer.png',
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
+                ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.delete),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
